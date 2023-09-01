75aa7da1a41d26289985be5be8405411770ed9b0
Baptiste Marcon <baptistemcn@icloud.com>
2023-09-01 22:16:45 +0200

    init

---
+++ Hello-Biding/Hello_BidingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_BidingApp.swift
+//  Hello-Biding
+//
+//  Created by Baptiste MARCON on 01/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hello_BidingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
